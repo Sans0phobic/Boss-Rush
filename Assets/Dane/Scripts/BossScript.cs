9bd7dd389efd57aaf93b38b1423e29c8e0578db7
Sans0phobic <dane.fournier@outlook.com>
2025-12-07 19:13:46 -0600

    Update BossScript.cs

--- Assets/Dane/Scripts/BossScript.cs
+++ Assets/Dane/Scripts/BossScript.cs
@@ -12,6 +12,8 @@ namespace DaneF
         [SerializeField] float bossSpeed = 5.0f;
 
         private float playerDistance;
+        private int rng = 0;
+        private float countdown;
 
         void Start()
         {
@@ -22,10 +24,35 @@ namespace DaneF
         {
             playerDistance = Vector3.Distance(transform.position, playerBody.transform.position);
             Debug.Log("Player Distance: " + playerDistance);
-            //Player melee range - 7m - Should ROAR here
-            //Boss should CHASE at 20m
-            //Boss should fire LASER at 30m
+            //Player melee range - 7m - Should ROAR here after a few seconds
+            //Boss should CHASE at 8 - 29m
+            //Boss should fire LASER or CHARGE at 30m
             //Boss should CLAW (technically bite) at 15m
+            //Boss should use FIRE PILLAR attack the next time they idle & are at 5% health or less
+
+            if (playerDistance <= 7)
+            {
+                Debug.Log("EnemyShockwaveState");
+                //Start timer 10 second timer using the 'countdown' variable
+                //countdown -= Time.deltaTime;
+                //if(countdown <= 0) {shockwave}
+            }
+            if (playerDistance > 7 && playerDistance <= 15) 
+            {
+                Debug.Log("EnemySwipeState");
+                //countdown = 10.0f;
+            }
+            if (playerDistance >15 && playerDistance <= 29)
+            {
+                Debug.Log("EnemyChaseState");
+            }
+            if (playerDistance >= 30)
+            {
+                if (rng == 0)
+                    Debug.Log("EnemyLaserState");
+                if (rng == 1)
+                    Debug.Log("EnemyChargeState");
+            }
         }
     }
 }
